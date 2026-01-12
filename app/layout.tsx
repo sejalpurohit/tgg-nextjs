87f306f3905ab083d236beb3e03a3fe97fadcb6e
Sejal Purohit <sejalpurohit3195@gmail.com>
2026-01-12 00:02:20 +0000

    fix viewport compile error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 import StickyHeader from "./components/StickyHeader";
@@ -8,9 +8,14 @@ import Footer from "./components/Footer";
 export const metadata: Metadata = {
   title: "PCP Pal",
   description: "PCP compensation checker",
-  viewport: "width=device-width, initial-scale=1",
 };
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 export default function RootLayout({
   children,
 }: {
